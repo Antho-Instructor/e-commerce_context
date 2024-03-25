9d9d4653e7b1c89640cd2305cc9cdbb744d0578f
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-03-25 11:14:39 +0100

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+	return (
+		<nav className="navbar">
+			<img src="https://picsum.photos/150/50" alt="logo" />
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Navbar;
